7f89cf66e24bab51ff0245b7cbf9b22673e16382
Patrick Massot <patrickmassot@free.fr>
2023-05-10 10:38:32 +0200

    Polish introduction

--- GlimpseOfLean/Introduction.lean
+++ GlimpseOfLean/Introduction.lean
@@ -1,18 +1,46 @@
 import Mathlib.Data.Real.Basic
-
 namespace Introduction
 
--- A sequence `u` of real numbers converges to `l` if `∀ ε > 0, ∃ N, ∀ n ≥ N, |u n - l| ≤ ε`
+/-
+# Introduction to this tutorial
+
+Welcome to this tutorial designed to give you a glimpse of Lean in a couple of hours.
+
+First let us see what a Lean proof looks like, without trying to understand any syntactical
+detail yet. You won't have anything to tuype in this file.
+You should be reading it in VSCode with it Lean 4 extension.
+
+If everything works, you currently see a panel to the right of this text with title
+"Lean Infoview", without much content yet, something like "No info found."
+This panel will start displaying interesting things inside the proof.
+First let us review two calculus definitions.
+-/
+
+/-- A sequence `u` of real numbers converges to `l` if `∀ ε > 0, ∃ N, ∀ n ≥ N, |u n - l| ≤ ε`.
+This condition will be spelled `seq_limit u l`. -/
 def seq_limit (u : ℕ → ℝ) (l : ℝ) : Prop :=
 ∀ ε > 0, ∃ N, ∀ n ≥ N, |u n - l| ≤ ε
 
--- A function`f : ℝ → ℝ` is continuous at `x₀` if
--- `∀ ε > 0, ∃ δ > 0, ∀ x, |x - x₀| ≤ δ ⇒ |f(x) - f(x₀)| ≤ ε`
+/- In the above definition, note that the `n`-th term of the sequence `u` is denoted
+simply by `u n`.
+
+Similarly, in the next definition, `f x` is what we would write `f(x)` on paper.
+Also note that implication is denoted by a single arrow (we'll explain why later). -/
+
+/-- A function`f : ℝ → ℝ` is continuous at `x₀` if
+`∀ ε > 0, ∃ δ > 0, ∀ x, |x - x₀| ≤ δ ⇒ |f(x) - f(x₀)| ≤ ε`.
+This condition will be spelled `continuous_at f x₀`.-/
 def continuous_at (f : ℝ → ℝ) (x₀ : ℝ) : Prop :=
 ∀ ε > 0, ∃ δ > 0, ∀ x, |x - x₀| ≤ δ → |f x - f x₀| ≤ ε
 
+/-- Now we claim that if `f` is continuous at `x₀` then it is sequentially continuous
+at `x₀`: for any sequence `u` converging to `x₀`, the sequence `f ∘ u` converges
+to `f x₀`.  -/
 example (f : ℝ → ℝ) (u : ℕ → ℝ) (x₀ : ℝ) (hu : seq_limit u x₀) (hf : continuous_at f x₀) :
-  seq_limit (f ∘ u) (f x₀) := by
+  seq_limit (f ∘ u) (f x₀) := by -- This `by` keyword marks the beginning of the proof
+  -- Put your text cursor here and watch the Lean InfoView panel to the right.
+  -- Then move your cursor from line to line in the proof while monitoring the Infoview.
+
   -- Our goal is to prove that, for any positive `ε`, there exists a natural
   -- number `N` such that, for any natural number `n` at least `N`,
   --  `|f(u_n) - f(x₀)|` is at most `ε`.
@@ -21,13 +49,10 @@ example (f : ℝ → ℝ) (u : ℕ → ℝ) (x₀ : ℝ) (hu : seq_limit u x₀)
   intros ε hε
   -- By assumption on `f` applied to this positive `ε`, we get a positive `δ`
   -- such that, for all real number `x`, if `|x - x₀| ≤ δ` then `|f(x) - f(x₀)| ≤ ε` (1).
-  -- On fixe un tel δ.
-  obtain ⟨δ, δ_pos, Hf⟩ : ∃ δ > 0, ∀ x, |x - x₀| ≤ δ → |f x - f x₀| ≤ ε
-  · exact hf ε hε
+  obtain ⟨δ, δ_pos, Hf⟩ : ∃ δ > 0, ∀ x, |x - x₀| ≤ δ → |f x - f x₀| ≤ ε := hf ε hε
   -- The assumption on `u` applied to this `δ` gives a natural number `N` such that
   -- for every natural number `n`, if `n ≥ N` then `|u_n - x₀| ≤ δ`   (2).
-  obtain ⟨N, Hu⟩ : ∃ N, ∀ n ≥ N, |u n - x₀| ≤ δ
-  · exact hu δ δ_pos
+  obtain ⟨N, Hu⟩ : ∃ N, ∀ n ≥ N, |u n - x₀| ≤ δ := hu δ δ_pos
   -- Let's prove `N` is suitable.
   use N
   -- Fix `n` which is at least `N`. Let's prove `|f(u_n) - f(x₀)| ≤ ε`.
@@ -36,3 +61,10 @@ example (f : ℝ → ℝ) (u : ℕ → ℝ) (x₀ : ℝ) (hu : seq_limit u x₀)
   apply Hf
   -- This follows from property (2) and our assumption on `n`.
   exact Hu n hn
+
+
+/-
+Now that this proof is over, you can use the file explorer to the left of this panel to
+open the first file of the `Exercises/Basics` folder. If the file explorer isn't shown, you
+can reveal by clicking the topmost icon from the icon column on the left.
+-/
