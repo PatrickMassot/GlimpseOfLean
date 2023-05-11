e968312749831b2623f66e23a879f134dad8417d
Patrick Massot <patrickmassot@free.fr>
2023-05-11 13:30:11 +0200

    Fix imports

--- GlimpseOfLean.lean
+++ GlimpseOfLean.lean
@@ -1,8 +1,8 @@
 import GlimpseOfLean.Introduction
 import GlimpseOfLean.Solutions.Basics.«01_equality_rewriting»
-import GlimpseOfLean.Solutions.Basics.«02_iff_if_and»
+import GlimpseOfLean.Solutions.Basics.«02_iff_if»
 import GlimpseOfLean.Solutions.Basics.«03_forall»
-import GlimpseOfLean.Solutions.Basics.«04_exists»
+import GlimpseOfLean.Solutions.Basics.«04_exists_and»
 import GlimpseOfLean.Solutions.Topics.SequenceLimits
 import GlimpseOfLean.Solutions.Topics.ChineseRemainder
 import GlimpseOfLean.Solutions.Topics.GaloisAdjunctions
