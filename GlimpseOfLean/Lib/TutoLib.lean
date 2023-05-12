399f99d769b0eb522e112ce9e91455cd7b258f85
Kyle Miller <kmill31415@gmail.com>
2023-05-12 12:48:38 +0000

    fancy Exists delaborator

--- GlimpseOfLean/Lib/TutoLib.lean
+++ GlimpseOfLean/Lib/TutoLib.lean
@@ -156,6 +156,49 @@ def infᵢ_delab : Delab := whenPPOption Lean.getPPNotation do
     | _ => pure stx
   return stx
 
+/-- The Exists notation has similar considerations as sup/inf -/
+@[delab app.Exists]
+def exists_delab : Delab := whenPPOption Lean.getPPNotation do
+  let #[ι, f] := (← SubExpr.getExpr).getAppArgs | failure
+  unless f.isLambda do failure
+  let prop ← Meta.isProp ι
+  let dep := f.bindingBody!.hasLooseBVar 0
+  let ppTypes ← getPPOption getPPFunBinderTypes
+  let stx ← SubExpr.withAppArg do
+    let dom ← SubExpr.withBindingDomain delab
+    withBindingBodyUnusedName $ fun x => do
+      let x : TSyntax `ident := .mk x
+      let body ← delab
+      if prop && !dep then
+        `(∃ (_ : $dom), $body)
+      else if prop || ppTypes then
+        `(∃ ($x:ident : $dom), $body)
+      else
+        `(∃ $x:ident, $body)
+  -- Cute binders
+  let stx : Term ←
+    match stx with
+    | `(∃ $i:ident, $j:ident ∈ $s ∧ $body)
+    | `(∃ ($i:ident : $_), $j:ident ∈ $s ∧ $body) =>
+      if i == j then `(∃ $i:ident ∈ $s, $body) else pure stx
+    | `(∃ $x:ident, $y:ident > $z ∧ $body)
+    | `(∃ ($x:ident : $_), $y:ident > $z ∧ $body) =>
+      if x == y then `(∃ $x:ident > $z, $body) else pure stx
+    | `(∃ $x:ident, $y:ident < $z ∧ $body)
+    | `(∃ ($x:ident : $_), $y:ident < $z ∧ $body) =>
+      if x == y then `(∃ $x:ident < $z, $body) else pure stx
+    | `(∃ $x:ident, $y:ident ≥ $z ∧ $body)
+    | `(∃ ($x:ident : $_), $y:ident ≥ $z ∧ $body) =>
+      if x == y then `(∃ $x:ident ≥ $z, $body) else pure stx
+    | `(∃ $x:ident, $y:ident ≤ $z ∧ $body)
+    | `(∃ ($x:ident : $_), $y:ident ≤ $z ∧ $body) =>
+      if x == y then `(∃ $x:ident ≤ $z, $body) else pure stx
+    | _ => pure stx
+  -- Merging
+  match stx with
+  | `(∃ $group:bracketedExplicitBinders, ∃ $groups*, $body) => `(∃ $group $groups*, $body)
+  | _ => pure stx
+
 end SupInfNotation
 
 namespace ProdProjNotation
