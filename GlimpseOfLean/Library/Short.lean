c309d0934621f5bf13169795dbb4762643bed360
Patrick Massot <patrickmassot@free.fr>
2025-02-26 14:31:31 +0100

    Get rid of annoying ring warning

--- GlimpseOfLean/Library/Short.lean
+++ GlimpseOfLean/Library/Short.lean
@@ -2,6 +2,10 @@ import Mathlib.Tactic
 import Mathlib.Data.Real.Basic
 import Mathlib.Data.Complex.Trigonometric
 
+@[inherit_doc Mathlib.Tactic.RingNF.ring]
+macro "ring" : tactic =>
+  `(tactic| first | ring1 | ring_nf)
+
 lemma ge_max_iff {α : Type*} [LinearOrder α] {p q r : α} : r ≥ max p q  ↔ r ≥ p ∧ r ≥ q :=
   max_le_iff
 
